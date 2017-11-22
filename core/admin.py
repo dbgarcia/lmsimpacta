298398e881aa906c4a8d4eb3c084a3f0c4e08053
pugaru <30847672+pugaru@users.noreply.github.com>
2017-11-22 20:01:37 -0200

    Add files via upload

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,46 @@
 from django.contrib import admin
-from core.models import Curso
+from django import forms
+from core.models import Curso,Aluno
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(Curso)
+class NovoAlunoForm(forms.ModelForm):
+    class Meta:
+        model = Aluno
+        fields = ('ra', 'nome','curso')
+
+    def save(self, commit=True):
+        user = super(NovoAlunoForm, self).save(commit = False)
+        user.set_password("123")
+        user.perfil = "A"
+        if commit:
+            user.save()
+        return user        
+
+class AlterarAlunoForm(forms.ModelForm):
+    class Meta:
+        model = Aluno
+        fields = ( 'nome', 'curso')
+
+class AlunoAdmin(UserAdmin):
+    form = AlterarAlunoForm
+    add_form = NovoAlunoForm
+    list_display = ('ra','nome', 'curso')
+    list_filter = ('perfil',)
+    fieldsets = ( (None, {'fields': ('ra','nome', 'curso')}),)
+    add_fieldsets = (
+        (None, {
+             'fields': ('ra','nome', 'curso')
+             }),
+        )
+    search_fields = ('ra',)
+    ordering = ('ra',)
+    filter_horizontal = ()
+
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('nome','tipo','carga_horaria')
+    list_filter = ('tipo',)
+
+
 # Register your models here.
+admin.site.register(Aluno,AlunoAdmin)
+admin.site.register(Curso,CursoAdmin)
--- lmsimpacta/urls.py
+++ lmsimpacta/urls.py
@@ -16,15 +16,17 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from core.views import *
+from django.contrib.auth.views import login,logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',index),
+    url(r'^$',index, name="home"),
     url(r'disciplina',disciplina),
     url(r'detalhe_curso',detalhe_curso),
     url(r'index',index),
     url(r'noticias',noticias),
     url(r'lista_curso',lista_cursos),
     url(r'contato',contato),
-    url(r'login',login),
+    url(r'entrar',login,{"template_name":"login.html"},name="login"),
+    url(r'sair',logout,{"template_name":"logout.html"}),
 ]
