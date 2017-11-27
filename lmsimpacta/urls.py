ebe2879c12a78241fe1d1ed4229732ed63f765f5
Douglas Garcia <douglas.garcia@ymail.com>
2017-11-27 10:33:58 -0200

    Update urls.py

--- lmsimpacta/urls.py
+++ lmsimpacta/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     url(r'^professor',professor,name='professor'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
