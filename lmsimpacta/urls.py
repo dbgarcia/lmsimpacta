69d37e7ad386286f7ca226ed152f82f6bc0453cd
Douglas Garcia <douglas.garcia@ymail.com>
2017-11-28 06:59:17 -0200

    Update urls.py

--- lmsimpacta/urls.py
+++ lmsimpacta/urls.py
@@ -20,8 +20,8 @@ urlpatterns = [
     url(r'sair',logout,{"template_name":"logout.html"}),
     url(r'aluno', aluno),
     url(r'professor',professor),
-#     url(r'boletim',boletim),
+    url(r'boletim',boletim),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
