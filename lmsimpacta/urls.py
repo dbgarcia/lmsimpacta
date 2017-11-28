73bab9e528b78175237fecc47d831e7ff008bf53
Douglas Garcia <douglas.garcia@ymail.com>
2017-11-28 02:56:37 -0200

    Update urls.py

--- lmsimpacta/urls.py
+++ lmsimpacta/urls.py
@@ -3,6 +3,8 @@
 from django.conf.urls import url
 from django.contrib import admin
 from core.views import *
+from django.conf import settings		
+from django.conf.urls.static import static
 from django.contrib.auth.views import login,logout
 
 urlpatterns = [
