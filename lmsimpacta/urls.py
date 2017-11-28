6339f281611920f145c4252e56fe9e30b6fa8c1c
pugaru <30847672+pugaru@users.noreply.github.com>
2017-11-28 19:16:21 -0200

    Add files via upload

--- lmsimpacta/urls.py
+++ lmsimpacta/urls.py
@@ -1,11 +1,24 @@
+"""lmsimpacta URL Configuration
 
-
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
 from django.conf.urls import url
 from django.contrib import admin
 from core.views import *
-from django.conf import settings		
+from django.contrib.auth.views import login,logout,password_change,password_change_done
 from django.conf.urls.static import static
-from django.contrib.auth.views import login,logout
+from django.conf import settings 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -18,10 +31,18 @@ urlpatterns = [
     url(r'contato',contato),
     url(r'entrar',login,{"template_name":"login.html"},name="login"),
     url(r'sair',logout,{"template_name":"logout.html"}),
-    url(r'aluno', aluno),
+    url(r'aluno',aluno),
     url(r'professor',professor),
-    url(r'boletim',boletim),
-]
+    url(r'^restrito/$',restrito,name="restrito"),
+    url(r'permission',permission),
+    url(r'^restrito/questao',questao_form,name="questao_form"),
 
+    url(r'^accounts/password/change/$', password_change, {
+        'template_name': 'registration/password_change_form.html'}, 
+        name='password_change'),
+    url(r'^accounts/password/change/done/$', password_change_done, 
+        {'template_name': 'registration/password_change_done.html'},
+        name='password_change_done'),
+]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
